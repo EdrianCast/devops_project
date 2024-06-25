ac066eb18f03fe44abc90f19d9a2a761e92c19d3
EdrianCast <AngelJuradoC@gmail.com>
2024-06-24 16:31:03 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# DevOps Test Project
+
+## Requisitos
+- Docker
+- Docker Compose
+
+## Configuración
+
+1. Clone the repository:
+   ```sh
+   git clone <your-repo-url>
+   cd devops_project
---
+++ lavarel/start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Wait for MySQL to be available
+echo "waiting for MySQL. . ."
+while ! nc -Z mysql 3306; do
+  sleep 1
+done
+echo "MySQL is up - executing command"
+
+# Executing migrations and seeder
+php artisan migrate --force
+php artisan db:seed --force
+
+# Start PHP-FPM
+php-fpm
